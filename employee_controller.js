d8a193210acca1f471abb820dee0d2ffac6782db
Javed Akhtar <javed.akhtar@gmail.com>
2019-04-25 17:27:57 +0530

    implemented controller for employee

---
+++ employee_controller.js
@@ -0,0 +1,9 @@
+module.exports = ({
+
+    listOfEmployees : function listOfEmployees (req, res, next) {
+        res.send('I am listOfEmployees')
+    },
+    singleEmployee : function listOfEmployees (req, res, next) {
+        res.send('I am singleEmployee')
+    }
+})
