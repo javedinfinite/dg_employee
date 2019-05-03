a3faab9e6c1718ea110e3838b58f284f075358f1
Javed Akhtar <javed.akhtar@gmail.com>
2019-05-03 20:52:25 +0530

    implemented code to allow multiple cors domain

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ var cors = require('cors')
 const app = express()
 var port = process.env.PORT || 3001;
 
-// var corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200
-//   }
 
-app.get('/', cors(), employeeController.getEmployees);
+var whitelist = ['http://localhost:3000', 'https://datagrokr-employee.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', employeeController.getEmployees);
 
 app.get('/:id', employeeController.getEmployee);
+
 
 app.listen(port, () => console.log(`Node App for datagrokr_employee is running on port ${port}!`));
 
